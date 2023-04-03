4adca5fc2e85b30a3bd418b2ce1065cbb5f391bb
Amir Kooshky's WSL <kooshkya@gmail.com>
2023-04-03 23:57:14 +0430

    create login basics

--- website/views.py
+++ website/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
+def index(request):
+    return HttpResponseRedirect(reverse('website:login'))
+
+def login_user(request):
+    context = {}
+    if request.method == "POST":
+        # process login attempt
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("website:index"))
+        else:
+            context['errors'] = ["Your login credentials are invalid."]
+    return render(request, "website/login.html", context=context)
+
+def register_user(request):
+    return HttpResponse("register page placeholder")
+
+def logout(request):
+    return None;
